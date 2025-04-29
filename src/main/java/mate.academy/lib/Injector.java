f1249ac13ceedba6a06a2f1fabe0a29ffd95203c
Sergii Nosachenko <54940595+sergii-nosachenko@users.noreply.github.com>
2025-04-29 19:33:36 +0300

    Implemented basic dependency injection: created @Component and @Inject annotations, marked services as components, added injection to ProductServiceImpl, implemented Injector with support for object creation and field injection Щойно

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,13 +1,72 @@
 package mate.academy.lib;
 
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Injector {
     private static final Injector injector = new Injector();
+    private final Map<Class<?>, Object> instances = new HashMap<>();
+
+    private Injector() {
+    }
 
     public static Injector getInjector() {
         return injector;
     }
 
     public Object getInstance(Class<?> interfaceClazz) {
-        return null;
+        Class<?> implClazz = findImplementation(interfaceClazz);
+
+        if (!implClazz.isAnnotationPresent(Component.class)) {
+            throw new RuntimeException("Missing @Component annotation on class: "
+                    + implClazz.getName());
+        }
+
+        Object instance = createInstance(implClazz);
+        injectDependencies(instance, implClazz);
+        return instance;
+    }
+
+    private Class<?> findImplementation(Class<?> interfaceClazz) {
+        if (interfaceClazz.equals(mate.academy.service.ProductParser.class)) {
+            return mate.academy.service.impl.ProductParserImpl.class;
+        } else if (interfaceClazz.equals(mate.academy.service.ProductService.class)) {
+            return mate.academy.service.impl.ProductServiceImpl.class;
+        } else if (interfaceClazz.equals(mate.academy.service.FileReaderService.class)) {
+            return mate.academy.service.impl.FileReaderServiceImpl.class;
+        } else {
+            throw new RuntimeException("No implementation found for: " + interfaceClazz.getName());
+        }
+    }
+
+    private Object createInstance(Class<?> clazz) {
+        if (instances.containsKey(clazz)) {
+            return instances.get(clazz);
+        }
+
+        try {
+            Object instance = clazz.getDeclaredConstructor().newInstance();
+            instances.put(clazz, instance);
+            return instance;
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to create instance of: " + clazz.getName(), e);
+        }
+    }
+
+    private void injectDependencies(Object instance, Class<?> clazz) {
+        for (Field field : clazz.getDeclaredFields()) {
+            if (field.isAnnotationPresent(Inject.class)) {
+                Class<?> fieldType = field.getType();
+                Object dependency = getInstance(fieldType);
+                field.setAccessible(true);
+                try {
+                    field.set(instance, dependency);
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException("Failed to inject dependency: "
+                            + fieldType.getName(), e);
+                }
+            }
+        }
     }
 }
